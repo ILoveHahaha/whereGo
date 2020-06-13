e38dac64324c7321278a5b54d81137717eef4ca1
ILoveHahaha <zhengbanbugu@163.com>
2020-06-13 23:58:11 +0800

    基本完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,8 +3,8 @@
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
-    <!--<home-recommend :list="recommendList"></home-recommend>-->
-    <!--<home-weekend :list="weekendList"></home-weekend>-->
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -12,18 +12,18 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
-// import HomeRecommend from './components/Recommend'
-// import HomeWeekend from './components/Weekend'
-// import axios from 'axios'
-// import { mapState } from 'vuex'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
-    // HomeRecommend,
-    // HomeWeekend
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   },
   data () {
     return {
@@ -34,14 +34,14 @@ export default {
       weekendList: []
     }
   },
-  // computed: {
-  //   ...mapState(['city'])
-  // },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
-    // getHomeInfo () {
-    //   axios.get('/api/index.json?city=' + this.city)
-    //     .then(this.getHomeInfoSucc)
-    // },
+    getHomeInfo () {
+      axios.get('/api/index.json?city=' + this.city)
+        .then(this.getHomeInfoSucc)
+    },
     getHomeInfoSucc (res) {
       res = res.data
       if (res.ret && res.data) {
@@ -54,15 +54,15 @@ export default {
     }
   },
   mounted () {
-    // this.lastCity = this.city
-    // this.getHomeInfo()
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
-  // activated () {
-  //   if (this.lastCity !== this.city) {
-  //     this.lastCity = this.city
-  //     this.getHomeInfo()
-  //   }
-  // }
 }
 </script>
 
